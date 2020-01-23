004f2e4910bf0a895c13adf964291e1ab2ec26f7
mcass19 <mcass19@hotmail.com>
2020-01-23 20:24:40 -0300

    Add type macro

---
+++ lib/typelixir/type.ex
@@ -0,0 +1,28 @@
+defmodule Type do
+  defmacro __using__(_options) do
+    quote do
+      import unquote(__MODULE__)
+    end
+  end
+
+  defmacro boolean(_) do end
+
+  defmacro string(_) do end
+
+  defmacro integer(_) do end
+
+  defmacro float(_) do end
+
+  defmacro atom(_) do end
+
+  defmacro list(_) do end
+  defmacro list(_, _) do end
+
+  defmacro tuple(_) do end
+  defmacro tuple(_, _) do end
+
+  defmacro map(_, _) do end
+  defmacro map(_, _, _) do end
+
+  defmacro functype(_, _, _) do end
+end
