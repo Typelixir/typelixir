0f803640ec18d88bf44bb82f8405cbddf2b537ee
mcass19 <mcass19@hotmail.com>
2020-01-25 17:53:51 -0300

    Add type comparator

---
+++ lib/typelixir/type_comparator.ex
@@ -0,0 +1,24 @@
+defmodule TypeComparator do
+  def less_or_equal(list_type1, list_type2) when (is_list(list_type1) and is_list(list_type2)) do
+    if (length(list_type1) === length(list_type2)), 
+      do: not (Enum.zip(list_type1, list_type2)
+          |> Enum.map(fn {type1, type2} -> less_or_equal(type1, type2) end)
+          |> Enum.member?(false)),
+      else: false
+  end
+
+  def less_or_equal({:map, {key_type1, value_type1}}, {:map, {key_type2, value_type2}}), 
+    do: less_or_equal(key_type1, key_type2) and less_or_equal(value_type1, value_type2)
+
+  def less_or_equal({:tuple, list_type1}, {:tuple, list_type2}), do: less_or_equal(list_type1, list_type2)
+
+  def less_or_equal({:list, type1}, {:list, type2}), do: less_or_equal(type1, type2)
+
+  def less_or_equal(:integer, :float), do: true
+  
+  def less_or_equal(type1, type2) when type1 === type2, do: true
+
+  def less_or_equal(nil, _), do: true
+
+  def less_or_equal(_, _), do: false
+end
