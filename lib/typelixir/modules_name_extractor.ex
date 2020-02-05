059b089c169fc9f265e869e72f0fd97ba91eaefc
mcass19 <mcass19@hotmail.com>
2020-02-04 21:03:24 -0300

    Refactor module name extractor

--- lib/typelixir/modules_name_extractor.ex
+++ lib/typelixir/modules_name_extractor.ex
@@ -1,28 +1,29 @@
 defmodule Typelixir.ModuleNamesExtractor do
   @moduledoc false
-
+  
   def extract_modules_names(all_paths) do
-    modules_paths = for path <- all_paths,
-      into: [],
-      do:
-        for module <- extract_module_names_file(path),
-        into: [],
-        do: {module, path}
-    modules_paths = Enum.into(Enum.concat(modules_paths), %{})
+    modules_paths = 
+      all_paths
+      |> Enum.map(fn path -> extract_module_names_file(path) end)
+      |> List.flatten()
+      |> Enum.into(%{})
 
     # while developing to see the info in the console
     IO.puts(["Modules map => ", inspect modules_paths])
-
     modules_paths
   end
 
   defp extract_module_names_file(path) do
-    ast = Code.string_to_quoted(File.read!(Path.absname(path)))
-    {_ast, result} = Macro.prewalk(ast, [], &extract(&1, &2))
-    result
+    path
+    |> Path.absname()
+    |> File.read!()
+    |> Code.string_to_quoted()
+    |> Macro.prewalk([], &extract(&1, &2))
+    |> elem(1)
+    |> Enum.map(fn module -> {module, path} end)
   end
 
   defp extract({:defmodule, _, [{:__aliases__, _, [module_name]}, _]} = elem, names), do: {elem, names ++ [module_name]}
 
   defp extract(elem, acc), do: {elem, acc}
-  end
+end
