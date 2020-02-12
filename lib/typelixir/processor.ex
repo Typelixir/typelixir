b15dd5eceee168ddb1b668740f8af2878f677abe
mcass19 <mcass19@hotmail.com>
2020-02-11 22:06:50 -0300

    Add pre processor to get the typed functions defined on the file

--- lib/typelixir/processor.ex
+++ lib/typelixir/processor.ex
@@ -1,19 +1,20 @@
 defmodule Typelixir.Processor do
   @moduledoc false
 
-  alias Typelixir.{TypeBuilder, TypeComparator}
+  alias Typelixir.{TypeBuilder, TypeComparator, PreProcessor}
 
   # FIRST
   # ---------------------------------------------------------------------------------------------------
 
   def process_file(path, env) do 
+    modules_functions = PreProcessor.process_file(path, env[:modules_functions])
     ast = Code.string_to_quoted(File.read!(Path.absname(path)))
     
     # while developing to see the info in the console
     IO.puts "#{path} ast:"
     IO.inspect ast
     
-    {_ast, result} = Macro.prewalk(ast, env, &process(&1, &2))
+    {_ast, result} = Macro.prewalk(ast, %{env | modules_functions: modules_functions}, &process(&1, &2))
     result
   end
 
@@ -29,9 +30,7 @@ defmodule Typelixir.Processor do
 
   # {:defmodule, _, MODULE}
   defp process({:defmodule, _, [{:__aliases__, _, [module_name]} | [[_tail]]]} = elem, env) do
-    env = %{env | module_name: module_name}
-    modules_functions = Map.put(env[:modules_functions], env[:module_name], Map.new())
-    {elem, %{env | modules_functions: modules_functions}}
+    {elem, %{env | module_name: module_name}}
   end
 
   # MODULES INTERACTION
@@ -61,21 +60,6 @@ defmodule Typelixir.Processor do
     else
       {elem, env}
     end
-  end
-
-  # FUNCTIONS
-  # ---------------------------------------------------------------------------------------------------
-
-  # {:@, _, [{:spec, _, [{:::, _, [{fn_name, _, [type_of_args]}, type_of_return]}]}]}
-  # {:typedfunc, _, [{fn_name, _, _}, args, type_of_return]}
-  defp process({:typedfunc, _, [{fn_name, _, _}, type_of_args, type_of_return]} = elem, env) do
-    type_of_args = Enum.map(type_of_args, fn type -> TypeBuilder.build(type, %{vars: env[:vars], mod_funcs: env[:modules_functions]}) end)
-    fn_type = {TypeBuilder.build(type_of_return, %{vars: env[:vars], mod_funcs: env[:modules_functions]}), type_of_args}
-
-    new_module_map = Map.put(env[:modules_functions][env[:module_name]], fn_name, fn_type)
-    modules_functions = Map.put(env[:modules_functions], env[:module_name], new_module_map)
-    
-    {elem, %{env | modules_functions: modules_functions}}
   end
 
   # VARIABLES
