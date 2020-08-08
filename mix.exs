0aa3fd8f32a6801e5376752f8dd6b7c50b656df7
mcass19 <mcass19@hotmail.com>
2020-08-08 13:47:47 -0300

    Add hex doc dependency

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule Typelixir.MixProject do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    []
+    [{:ex_doc, ">= 0.0.0", only: :dev, runtime: false}]
   end
 
   defp package do
